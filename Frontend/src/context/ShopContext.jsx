e86f34511835024ceb81f2ab4aaeea5783f3f676
Naresh Khatri <nareshkhatri9810@gmail.com>
2024-09-18 23:34:50 +0545

    implement the add to cart functionality

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,24 +1,77 @@
-import { createContext, useState } from 'react'
-import {products} from '../assets/assets'
+import { createContext, useEffect, useState } from 'react'
+import { products } from '../assets/assets'
+import { toast } from 'react-toastify';
 export const ShopContext = createContext();
 
 
-const ShopContextProvider = (props) =>
-{
-    const currency ='Rs';
+const ShopContextProvider = (props) => {
+    const currency = 'Rs';
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
 
-    const value =
-    {
-        products, currency, delivery_fee,
-        search, setSearch,showSearch,setShowSearch
+    // logic for add to cart...
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+          if(!size)
+          {
+            toast.error("Please select the size");
+            return;
+          }
+
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+
+            }
+            else {
+                cartData[itemId][size] = 1;
+
+            }
+        }
+        else
+        {
+            cartData[itemId] ={};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData)
 
     }
 
-    return(
-        <ShopContext.Provider value ={value}>
+ 
+
+     const getCartCount = () =>{
+        let totalCount = 0;
+        for(const items in cartItems)
+        {
+            for( const item in cartItems[items])
+            {
+                try{
+                    if(cartItems[items][item] > 0)
+                    {
+                        totalCount += cartItems[items][item];
+                    }
+
+                }catch(error)
+                {
+
+                }
+            }
+        }
+        return totalCount;
+     }
+
+     const value =
+     {
+         products, currency, delivery_fee,
+         search, setSearch, showSearch, setShowSearch,
+         cartItems, addToCart,getCartCount
+ 
+     }
+    return (
+        <ShopContext.Provider value={value}>
             {props.children}
         </ShopContext.Provider>
     )
